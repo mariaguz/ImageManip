6d12b03c21699252985e77b3e64506075b870b23
Maria Guz <mania.vicodin@gmail.com>
2022-10-19 19:08:43 +0200

    minor changes

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import numpy as np
 from PIL import Image
 import os
 
+path = "M:\DOKUMENTY\KUBA HALT\sesja_na_insta"
 
-def padding(img):
+
+def padding(img, i, rgb):
     # setting up new size of the file (need to be square)
     old_width, old_height = img.size
     diff = old_width - old_height
@@ -20,12 +22,19 @@ def padding(img):
 
     # setting up the padding size
 
-    padding_left = int((new_width - old_width) / 2)
+    left = [1, 3, 7, 9]
+    right = [4, 6, 10, 12]
+    center = [2, 8]
+    if (i in left):
+        padding_left = int((new_width - old_width) / 4)
+    elif (i in right):
+        padding_left = int(((new_width - old_width) / 4) * 3)
+    else:
+        padding_left = int((new_width - old_width) / 2)
+
     padding_top = int((new_height - old_height) / 2)
 
-    #
-    result = Image.new(img.mode, (new_width, new_height), (0, 0, 0))
-    #
+    result = Image.new(img.mode, (new_width, new_height), rgb)
     result.paste(img, (padding_left, padding_top))
     return result
 
@@ -33,10 +42,11 @@ def padding(img):
 # opening all images from folder
 images = []
 
-for f in glob.iglob("M:\DOKUMENTY\KUBA HALT\sesja\*"):
+for f in glob.iglob(path + "\*"):
     images.append(Image.open(f))
 
 i = 0
 for img in images:
     i += 1
-    padding(img).save(r"M:\DOKUMENTY\KUBA HALT\sesja\padding_%d.jpg" % i)
+    padding(img, i, (0, 0, 0)).save(path + "\padding_black_%d.jpg" % i)
+    padding(img, i, (255, 255, 255)).save(path + "\padding_white_%d.jpg" % i)
