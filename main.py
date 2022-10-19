fa9daa06be214ea7c80ba820995f9d833c9fe2ba
Maria Guz <mania.vicodin@gmail.com>
2022-10-19 17:47:33 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import glob
+import numpy as np
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# opening all images from folder
+images = []
+for f in glob.iglob("M:\DOKUMENTY\KUBA HALT\sesja\*"):
+    images.append(Image.open(f))
 
+for img in images:
+    # img.show()
+    # setting up new size of the file (need to be square)
+    old_width, old_height = img.size
+    diff = old_width-old_height
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # if (diff < 0 ):
+    #     new_width = old_width + abs(diff)
+    #     new_height = old_height
+    # else:
+    #     new_height = old_height + abs(diff)
+    #     new_width = old_width
+    #
+    #
+    # padding_right = int((new_width - old_width) /2)
+    # padding_left = int((new_width - old_width) /2)
+    # padding_top = int((new_height - old_height) /2)
+    # padding_bottom = int((new_height - old_height) /2)
+    #
+    # #
+    # result = Image.new(img.mode, (new_width, new_height), (0, 0, 0))
+    # #
+    # result.paste(img, (padding_left, padding_top))
+    # result.show()
